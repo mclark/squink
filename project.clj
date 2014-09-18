dd02980cdb739fb80c72e84c75d655c4c498983d
Matt Clark <44023+mclark@users.noreply.github.com>
2014-09-17 22:21:48 -0400

    removed liberator dep. sanitized input. transactionalized db interaction.

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler squink.core/handler}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [liberator "0.12.1"]
                  [compojure "1.1.9"]
                  [ring/ring-core "1.3.1"]
                  [org.clojure/java.jdbc "0.3.5"]
-                 [mysql/mysql-connector-java "5.1.25"]])
+                 [mysql/mysql-connector-java "5.1.25"]
+                 [clojurewerkz/urly "1.0.0"]])
--- src/squink/core.clj
+++ src/squink/core.clj
@@ -1,8 +1,10 @@
 (ns squink.core
   (:require [clojure.java.jdbc :as jdbc]
-            [clojure.string :refer [blank?]]
+            [clojure.string :refer [blank? trim]]
             [ring.middleware.params :refer [wrap-params]]
-            [compojure.core :refer [defroutes GET POST]])
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [compojure.core :refer [defroutes GET POST]]
+            [clojurewerkz.urly.core :refer [url-like]])
   (:import clojure.lang.Murmur3))
 
 (def db {:subprotocol "mysql"
@@ -23,44 +25,58 @@
 
 (def hostname "squi.nk")
 
-(defn find-url [hashed]
-  (first (jdbc/query db ["SELECT url FROM shortened_url WHERE hashed=?" hashed]
+(defn find-url [conn hashed]
+  (first (jdbc/query conn ["SELECT url FROM shortened_url WHERE hashed=?" hashed]
                      :row-fn :url)))
 
-(defn insert-url [url hashed]
-            (jdbc/insert! db :shortened_url
+(defn insert-url [conn url hashed]
+            (jdbc/insert! conn :shortened_url
                           {:url url :hashed hashed})
-  hashed)
+  true)
 
 (def hash-url (comp (partial format "%x")
                     #(Murmur3/hashUnencodedChars %)))
 
+(defn persist [url hashed]
+  (jdbc/with-db-transaction
+    [conn db]
+    (let [existing (find-url conn hashed)]
+      (if (nil? existing) (insert-url conn url hashed) (= existing url)))))
+
 (defn shorten [url]
   (loop [candidate-url url tries-remaining 3]
     (let [hashed (hash-url candidate-url)
-          existing (find-url hashed)]
-      (if (nil? existing)
-        (insert-url url hashed)
-        (if (= existing url)
-          hashed
-          (when
-            (< 0 tries-remaining)
-            (recur (str tries-remaining "+" url) (dec tries-remaining))))))))
+          synced-to-db? (persist candidate-url hashed)]
+      (if (and (< 0 tries-remaining) (not synced-to-db?))
+        (recur (str tries-remaining "+" url) (dec tries-remaining))
+        hashed))))
 
-(defn valid? [url]
+(defn- sanitize-url [^String url]
+  (if (blank? url)
+    nil
+    (.toString (url-like url))))
+
+(defn- valid? [url]
   (and (not (blank? url)) (> 2000 (.length url))))
 
+(defn handle-create [url]
+  (let [cleaned-url (sanitize-url url)]
+    (if (valid? cleaned-url)
+      {:status 201
+       :body   (str hostname "/" (shorten cleaned-url))}
+      {:status 400})))
+
+(defn handle-lookup [hash]
+  (let [url (find-url db hash)]
+    (if url {:status 301 :headers {"Location" url}}
+            {:status 404})))
+
 (defroutes app
-           (GET "/:hash" [hash]
-                (let [url (find-url hash)]
-                  (if url {:status 301 :headers {"Location" url}}
-                          {:status 404})))
-           (POST "/" {{url "url"} :params}
-                 (if (valid? url)
-                   {:status 201
-                    :body   (str hostname "/" (shorten url))}
-                   {:status 400})))
+           (GET "/:hash" [hash] (handle-lookup hash))
+           (POST "/" {{url :url} :params}
+                 (handle-create url)))
 
 (def handler
   (-> app
+      wrap-keyword-params
       wrap-params))
